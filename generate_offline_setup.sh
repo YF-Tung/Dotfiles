2135d0a3e21ad2b7c63c58fb63a7f6fbfeb82976
Yu-Fan Tung <Yufan.Tung@gmail.com>
2019-03-05 13:25:21 +0800

    Add source command into offline setup

--- generate_offline_setup.sh
+++ generate_offline_setup.sh
@@ -7,6 +7,6 @@ trap "rm -rf $tempDir" EXIT
 tar zcf ${tempDir}/dot.tgz dot
 bTxt=$(base64 -i ${tempDir}/dot.tgz)
 
-echo "temp=\`mktemp -d\` && cd \$temp && echo ${bTxt} | base64 -D -o dot.tgz && tar xf dot.tgz && dot/install.sh --noninteractive" > setup_offline.sh
+echo "temp=\`mktemp -d\` && cd \$temp && echo ${bTxt} | base64 -D -o dot.tgz && tar xf dot.tgz && dot/install.sh --noninteractive && source ~/.bashrc" > setup_offline.sh
 chmod +x setup_offline.sh
 
