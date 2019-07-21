f9b4d66af17f7a0dd63e8c1b2117f77c00c2c61e
Yu-Fan Tung <yufantung@gmail.com>
2019-07-21 14:12:37 +0800

    Fix generate_offline_setup issue

--- generate_offline_setup.sh
+++ generate_offline_setup.sh
@@ -9,6 +9,6 @@ export LANG=zh_TW.UTF-8
 tar zcf ${tempDir}/dot.tgz dot
 bTxt=$(base64 -i ${tempDir}/dot.tgz)
 
-echo "temp=\`mktemp -d\` && cd \$temp && echo ${bTxt} | base64 --decode > dot.tgz && tar xf dot.tgz && dot/install.sh --noninteractive && source ~/.bashrc" > setup_offline.sh
+echo "temp=\`mktemp -d\` && pushd \$temp && echo ${bTxt} | base64 --decode > dot.tgz && tar xf dot.tgz && dot/install.sh --noninteractive && source ~/.bashrc && popd && rm -rf \$temp" > setup_offline.sh
 chmod +x setup_offline.sh
 
