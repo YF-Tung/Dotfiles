f4dc80219eb423a1effddb768862cce3457391f1
Yu-Fan Tung <yufantung@gmail.com>
2019-07-21 14:10:17 +0800

    Don't install Vundle

--- dot/install.sh
+++ dot/install.sh
@@ -58,9 +58,9 @@ rm -rf ~/.gitconfig.bak
 
 # 3. Install vim plugin
 cd ~
-if [ ! -d ".vim/bundle/Vundle.vim" ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
+#if [ ! -d ".vim/bundle/Vundle.vim" ]; then
+#    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#fi
 
 # 4. Done
 source ~/.bashrc
